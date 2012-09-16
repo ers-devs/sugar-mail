a2ed8391858935cbf8527b01eb406a7cb522601f
Christophe Gueret <christophe.gueret@gmail.com>
2012-09-16 21:48:23 +0200

    More code for the replication service, almost done :)

--- src/Model.py
+++ src/Model.py
@@ -15,6 +15,8 @@ class Messages(object):
 		graph = graphstore.get_instance().create_object()
 		entry = graph.create_entry(uid=None, category='Message')
 		entry.add("message", Literal(message))
-		entry.add("visibility", util.public_uri())
+		graph.add_share(util.public_uri())
 		graphstore.get_instance().persist_object(graph)
 	
+	def get_messages(self):
+		pass
