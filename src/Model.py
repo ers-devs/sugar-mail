70be134505f55e16952e0b001e6f1e6c1f20efd8
Marat Charlaganov <charlaganov@gmail.com>
2013-08-30 16:11:06 +0200

    Use sioc and urn's

--- src/Model.py
+++ src/Model.py
@@ -1,10 +1,6 @@
-'''
-Created on 15 Sep 2012
-
-@author: cgueret
-'''
-import uuid
-import time
+import random
+from time import gmtime, strftime
+from calendar import timegm
 from sugar import profile
 from rdflib import Literal, Namespace
 from ers import ERSLocal
@@ -18,30 +14,35 @@ class Messages(object):
         Constructor
         '''
         self.registry = ERSLocal()
+
+    def _new_id(self, part_id):
+        return "urn:ers:act:messages:{}:{:04x}".format(part_id, random.randrange(16**4))
     
     def post(self, message):
         '''
         Post a new public message
         '''
-        uid = str(uuid.uuid4())
-        self.registry.add_data(uid, "message", Literal(message), "g1")
-        self.registry.add_data(uid, "author", Literal(profile.get_nick_name()), "g1")
-        self.registry.add_data(uid, "share", OLPC_RESOURCE['public'], "g1")
-        self.registry.add_data(uid, "category", "Message", "g1")
-        self.registry.add_data(uid, "time", time.time(), "g1")
+        utc_time = gmtime()
+        entity = self._new_id(timegm(utc_time))
+        timestamp = strftime("%Y-%m-%dT%H:%M:%SZ", utc_time)
+        self.registry.add_data(entity, "dcterms:created", timestamp, "g1")
+        self.registry.add_data(entity, "sioc:has_creator", Literal(profile.get_nick_name()), "g1")
+        self.registry.add_data(entity, "sioc:content", Literal(message), "g1")
+        self.registry.add_data(entity, "sioc:addressed_to", OLPC_RESOURCE['public'], "g1")
+        self.registry.add_data(entity, "rdf:type", "sioc:MailMessage", "g1")
     
     def get_messages(self):
         '''
         Get the list of messages currently stored locally
         '''
         messages = []
-        for (entity, graph) in self.registry.search("category", "Message"):
+        for (entity, graph) in self.registry.search("rdf:type", "sioc:MailMessage"):
             try:
                 data = self.registry.get_annotation(entity)
-                message = ', '.join(data['message'])
-                author = ', '.join(data['author'])
-                timestamp = data['time'][-1]
+                message = ', '.join(data["sioc:content"])
+                author = ', '.join(data["sioc:has_creator"])
+                timestamp = data["dcterms:created"][-1]
                 messages.append((timestamp, "{0} (from: {1})".format(message, author)))
             except Exception as e:
-                messages.append((0, "fail {0}".format(e)))
+                messages.append((0, "error: {0}".format(e)))
         return [m for d, m in sorted(messages)]
